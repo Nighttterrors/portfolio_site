1071052d5f392ee2f08461fece8dad4195f44701
Nighttterrors <diegogutierrez159@gmail.com>
2026-03-08 15:49:18 -0500

    Update settings.py

--- portfolio_site/settings.py
+++ portfolio_site/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
-#'django-insecure-8#kh2@d$)uh=ub9p@$zo=5kzl%pza$&5*$jnhcpfcjz(4+@o4r' 
+#'' 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
