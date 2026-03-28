199bc0d436cf3606b27f99931870d7fbffc2a98e
Nighttterrors <diegogutierrez159@gmail.com>
2026-03-27 22:04:07 -0500

    fix: update settings.py

--- portfolio_site/settings.py
+++ portfolio_site/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["https://diegos-corner.onrender.com"]
+ALLOWED_HOSTS = ["diegos-corner.onrender.com"]
 
 # Application definition
 
