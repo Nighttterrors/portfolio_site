e84c88d354bdd3468ef6934ffebb7b2e244991b0
Nighttterrors <diegogutierrez159@gmail.com>
2026-04-10 22:34:04 -0500

    added to the allowed hosts

--- portfolio_site/settings.py
+++ portfolio_site/settings.py
@@ -29,7 +29,11 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["diegos-corner.onrender.com", '127.0.0.1']
+ALLOWED_HOSTS = [
+    "diegos-corner.onrender.com", 
+    '127.0.0.1',
+    'diegogutierrez.tech',
+    'www.diegogutierrez.tech',]
 CSRF_TRUSTED_ORIGINS = ["https://diegos-corner.onrender.com"]
 
 # Application definition
