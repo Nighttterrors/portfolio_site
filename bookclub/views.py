d5696e879e8c6c2d4c14c134ff248703522c6e5d
Nighttterrors <diegogutierrez159@gmail.com>
2026-04-29 20:57:35 -0500

    Added user review submissions

--- bookclub/views.py
+++ bookclub/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
-from .models import Book
+from django.contrib import messages
+from django.db import IntegrityError
 from django.contrib.auth.decorators import login_required
 
+from .models import Book
+from .forms import ReviewForm
 
 
 
@@ -10,10 +13,37 @@ from django.contrib.auth.decorators import login_required
 def book_home(request):
     curBook = Book.objects.order_by('-month').first() 
     reviews = curBook.reviews.all() if curBook else []
+    
+    #Check if the user has already reviewed the book
+    existingReview =  None
+    if curBook:
+        existingReview = curBook.reviews.filter(user=request.user).first()
+
+    form = ReviewForm()
+
+    if request.method == "POST" and curBook:
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            if existingReview:
+                messages.error(request, "You have already submitted a review! 👌")
+                return redirect("book_home")
+            review = form.save(commit=False)
+            review.user = request.user
+            review.book = curBook
+
+            try:
+                review.save()
+                messages.success(request, "Review Submitted")
+            except IntegrityError:
+                messages.error(request, "You already reviewed this book.")
+
+            return redirect("book_home")
 
     return render(request, "bookclub/bookclubindex.html" , {
         "book" : curBook,
         "reviews" : reviews,
+        "form" : form,
+        "existingReview" : existingReview
     })
 
 
