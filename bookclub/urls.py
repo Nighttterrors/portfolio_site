c623a45c06d707161d5e5ac4e9693e8d20591745
Nighttterrors <diegogutierrez159@gmail.com>
2026-05-03 21:06:40 -0500

    Fixed routes

--- bookclub/urls.py
+++ bookclub/urls.py
@@ -11,5 +11,4 @@ urlpatterns = [
     path('signup/' , views.sign_up, name='signup'),
     # path('login/', auth_views.LoginView.as_view(template_name='bookclub/login.html'), name='login'),
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
