04a71117dfb105e9b5087bf8e806ebb7a4e233b8
Nighttterrors <diegogutierrez159@gmail.com>
2026-05-10 21:19:58 -0500

    fixed css issues and html errors

--- portfolio_site/settings.py
+++ portfolio_site/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 #'' 
 # SECURtttTY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
 
