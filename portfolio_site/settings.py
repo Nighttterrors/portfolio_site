3330c19d8eb078f0b85b7296569650d53928de4d
Nighttterrors <diegogutierrez159@gmail.com>
2026-05-25 00:17:52 -0500

    Fixed typo

--- portfolio_site/settings.py
+++ portfolio_site/settings.py
@@ -33,7 +33,7 @@ DEBUG = False
 
 
 ALLOWED_HOSTS = [
-    "diegos-corner.onrender.com", S
+    "diegos-corner.onrender.com", 
     '127.0.0.1',
     'diegogutierrez.tech',
     'www.diegogutierrez.tech',]
