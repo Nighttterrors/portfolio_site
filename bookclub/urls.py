6602c24d9aa10e6015b6ea5318bcb6efa7f79d05
Nighttterrors <diegogutierrez159@gmail.com>
2026-06-21 08:31:13 -0500

    Add toggle like feature for forum posts; update URL patterns

--- bookclub/urls.py
+++ bookclub/urls.py
@@ -18,7 +18,8 @@ urlpatterns = [
     path('delete-member/<int:user_id>/', views.delete_member, name='delete_member'),
     path('forum/', views.forum, name='forum'),
     path('login/', views.custom_login, name='custom_login'),
-    path('<int:book_id>/archive/', views.book_archive, name='book_archive')
+    path('<int:book_id>/archive/', views.book_archive, name='book_archive'),
+    path('forum/<int:post_id>/like/', views.toggle_like, name='toggle_like')
 
     
     # path('login/', auth_views.LoginView.as_view(template_name='bookclub/login.html'), name='login'),
